9c1515de2ab33e906c5986f252cbe5a6a8370b59
illumination <42507764+ilumn@users.noreply.github.com>
2022-02-28 11:45:01 -0500

    Created

---
+++ Question-Answering.py
@@ -0,0 +1,18 @@
+import openai
+
+key = input("please paste openai api key:")
+
+openai.api_key = os.getenv(key)
+
+context = input("input the topic that the question pertains to, to provide the ai with context:")
+question = input("please input the question you would like answered:")
+
+response = openai.Completion.create(
+  engine="text-curie-001",
+  prompt="Answer this question about"+context+"\n\n"+question,
+  temperature=0.7,
+  max_tokens=64,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)

74cdec7c417fbeb98ca16ff9d6266ff7f23f09da
illumination <42507764+ilumn@users.noreply.github.com>
2022-03-16 10:01:33 -0400

    it actually runs now

--- Question-Answering.py
+++ Question-Answering.py
@@ -1,18 +1,28 @@
 import openai
+import keyboard
+import json
 
 key = input("please paste openai api key:")
 
-openai.api_key = os.getenv(key)
+openai.api_key = key
 
-context = input("input the topic that the question pertains to, to provide the ai with context:")
-question = input("please input the question you would like answered:")
+def main():
+  context = input("input the topic that the question pertains to, to provide the ai with context:")
+  question = input("please input the question you would like answered:")
 
-response = openai.Completion.create(
-  engine="text-curie-001",
-  prompt="Answer this question about"+context+"\n\n"+question,
-  temperature=0.7,
-  max_tokens=64,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-)
+  response = openai.Completion.create(
+    engine="text-curie-001",
+    prompt="Answer this question about"+context+"\n\n"+question,
+    temperature=0.7,
+    max_tokens=64,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+  )
+
+  print(response)  
+
+main()
+
+print('press esc to exit')
+keyboard.add_hotkey('esc',exit())
